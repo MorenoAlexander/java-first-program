aa20784fc68201b48e3fdd927d6f77bc0553b1cd
Alexander Moreno <am@alexandermoreno.com>
2021-11-21 23:41:57 -0600

    added main method implementation

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,5 +1,7 @@
 package com.h2;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,7 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+
+
     }
 
     public static int doubleTheNumber(int number) {
