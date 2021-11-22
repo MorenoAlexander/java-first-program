16683cc1e3ae5df09b468af18e683681797d05a7
am17 <am171945@live.com>
2021-11-21 23:14:36 -0600

    Added main stub for now

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -3,4 +3,11 @@ package com.h2;
 
 public class BestLoanRates {
 
+	public static final Map<Integer,Float> bestRates = new Map<Integer, Float>() {}
+
+	public static void main(String[] args) {
+		
+	}
+
+	
 }
