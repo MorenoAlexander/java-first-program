726827001afa169ba752b931efda8969c5817a7f
Alexander Moreno <am@alexandermoreno.com>
2021-12-02 23:30:33 -0600

    added sumOfDebits and sumOfCredits functions in SavingsCaldulater.java

---
+++ src/main/java/com/h2/SavingsCaldulater.java
@@ -0,0 +1,30 @@
+package com.h2;
+
+public class SavingsCaldulater {
+    private float[] credits;
+    private float[] debits;
+
+    public SavingsCaldulater(float[] credits, float[] debits) {
+        this.credits = credits;
+        this.debits = debits;
+    }
+
+    private float sumOfCredits() {
+        float sum = 0.0f;
+        for (float credit : credits) {
+            sum += credit;
+        }
+        return sum;
+
+    }
+
+    private float sumOfDebits() {
+        float sum = 0.0f;
+        for (float debit : this.debits) {
+            sum += debit;
+        }
+        return sum;
+    }
+
+
+}
