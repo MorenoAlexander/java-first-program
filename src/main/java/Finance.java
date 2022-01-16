90841cfb81cb0be5d73f20a8e85a895c4144ba66
Alexander Moreno <am@alexandermoreno.com>
2022-01-16 00:16:16 -0600

    added switch case
    Signed-off-by: Alexander Moreno <am@alexandermoreno.com>

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -8,11 +8,21 @@ public class Finance {
 
     public final static Map<String, String> commandsToUsage = Map.of(
             BEST_LOAN_RATES, "usage: bestLoanRates",
-            SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ',' <debits separated by ','>",
+            SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
             MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
     );
 
     private static boolean validateCommandArguments(String args[]) {
-        return false;
+        switch (args[0]) {
+            case BEST_LOAN_RATES:
+                return args.length == 1;
+
+            case SAVINGS_CALCULATOR:
+                return args.length == 3;
+            case MORTGAGE_CALCULATOR:
+                return args.length == 4;
+            default:
+                return false;
+        }
     }
 }
