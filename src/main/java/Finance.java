a716fa284be992bbc08d5bdb3bd2532b76fb11ee
Alexander Moreno <am@alexandermoreno.com>
2022-01-16 00:41:09 -0600

    implemented executeCommand() added main method stub
    Signed-off-by: Alexander Moreno <am@alexandermoreno.com>

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,10 +1,19 @@
 
+import com.h2.BestLoanRates;
+import com.h2.MortgageCalculator;
+import com.h2.SavingsCalculator;
+
 import java.util.Map;
 
 public class Finance {
     public final static String BEST_LOAN_RATES = "bestLoanRates";
     public final static String SAVINGS_CALCULATOR = "savingsCalculator";
     public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
+
+
+    public static void Main(String args[]) {
+
+    }
 
     public final static Map<String, String> commandsToUsage = Map.of(
             BEST_LOAN_RATES, "usage: bestLoanRates",
@@ -16,13 +25,30 @@ public class Finance {
         switch (args[0]) {
             case BEST_LOAN_RATES:
                 return args.length == 1;
-
             case SAVINGS_CALCULATOR:
                 return args.length == 3;
             case MORTGAGE_CALCULATOR:
                 return args.length == 4;
             default:
                 return false;
+        }
+    }
+
+    private static void executeCommand(String command, String arguments[]) {
+        switch (command) {
+            case BEST_LOAN_RATES:
+                System.out.println("Finding best loan rates ...");
+                BestLoanRates.main(arguments);
+                return;
+            case SAVINGS_CALCULATOR:
+                System.out.println("Finding your net savings ...");
+                SavingsCalculator.main(arguments);
+                return;
+            case MORTGAGE_CALCULATOR:
+                System.out.println("Finding your monthly payment ...");
+                MortgageCalculator.main(arguments);
+                return;
+
         }
     }
 }
